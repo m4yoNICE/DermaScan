7491e4c8dd9b40bde3eff16b1c13b648b4685b17
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2025-10-18 13:58:58 +0800

    updating gemini.ai to automatically find newer photos.

--- Backend/ai/SkinClassifierAI/skin_classify_gemini.py
+++ Backend/ai/SkinClassifierAI/skin_classify_gemini.py
@@ -1,3 +1,4 @@
+import os
 import google.generativeai as genai
 from PIL import Image
 
@@ -9,12 +10,18 @@ genai.configure(api_key="AIzaSyCm6zU5e4mY-SOF61C75zU8Zno1uQTh9Ns")
 # --- Choose the Gemini model ---
 model = genai.GenerativeModel("models/gemini-2.5-pro")
 
-img_path = "../../skinUploads/Eczema.jpg"
+img_path = "../../skinUploads"
+
+latest_file = max(
+    [os.path.join(img_path, f) for f in os.listdir(img_path)],
+    key=os.path.getmtime
+)
+
 img = Image.open(img_path)
 
 prompt = """
 You are a dermatology assistant.
-Classify this skin image into eczema, acne, psoriasis, or normal.
+Classify this skin image based {latest_file} into eczema, acne, psoriasis, or normal.
 Estimate your subjective confidence (1–10) and explain briefly.
 Return your answer in JSON:
 {
