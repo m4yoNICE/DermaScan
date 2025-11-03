d19fd0f64f87458de4295fc27d40320e01a36aec
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2025-11-03 16:20:33 +0800

    fixing and moving files from the global backend to locally stored on the adminsBE.

---
+++ Backend/AdminBE/adminauth/adminAuthController.js
@@ -0,0 +1,57 @@
+import { findUserByEmail, createUser } from "../services/authServices.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { ENV } from "../config/env.js";
+
+ export async function login(req, res) {
+ try {
+    const { email, password } = req.body;
+    const user = await findUserByEmail(email);
+    if (!user) {
+      return res.status(401).json({ error: "Invalid Credentials" });
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const payload = { id: user.id, email: user.email };
+    const isAdmin = user.isAdmin || false;
+    if (!isAdmin) {
+      return res.status(403).json({ error: "Access denied" });
+    } else {
+    const token = jwt.sign(payload, ENV.JWT_SECRET, { expiresIn: "15m" });
+    res.status(200).json({
+      message: "Login successful",
+      user: { id: user.id, email: user.email },
+      token,
+    });
+  }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+}
+
+export async function register(req, res) {
+  try {
+    const { email, firstname, dob, lastname, password } = req.body;
+    const existingUser = await findUserByEmail(email);
+    if (existingUser) {
+      return res.status(400).json({ error: "Email already registered" });
+    }
+    const newUser = await createUser(email, firstname, dob, lastname, password);
+
+    // Immediately issue token (same as login)
+    const payload = { id: newUser.id, email: newUser.email };
+    const token = jwt.sign(payload, ENV.JWT_SECRET, { expiresIn: "15m" });
+
+    res.status(201).json({
+      message: "Registration successful",
+      user: { id: newUser.id, email: newUser.email },
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+}
--- Backend/AdminBE/adminservices/fetchUsers.js
+++ Backend/AdminBE/adminservices/fetchUsers.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { User } from "../models/User.js"; 
+import { User } from "../../src/models/User.js"; 
 
 const router = express.Router();
 
