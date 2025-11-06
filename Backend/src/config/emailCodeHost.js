afcba83c863c2b4f7cb493a33a7a918a46982ac5
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2025-11-06 13:40:58 +0800

    adding email verification script + automation for emain sending code

---
+++ Backend/src/config/emailCodeHost.js
@@ -0,0 +1,12 @@
+export default {
+  play: {
+    mailer: {
+      host: "live.smtp.mailtrap.io",
+      port: 587,
+      ssl: false,
+      tls: true,
+      user: "api",
+      password: "3dedc098db1c49b59fca18508c0391aa" 
+    }
+  }
+};
