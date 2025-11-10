f6a59e5a94458473f089a8d80ade5b3caa012b18
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2025-11-10 14:02:38 +0800

    TASK 02: Reverting old codes

--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -1,13 +1,13 @@
 import "dotenv/config"
 
 export const ENV = {
-    PORT=3000,
-    DB_HOST= 127.0.0.1,
-    DB_PORT=3306,
-    DB_DATABASE=derma_care,
-    DB_USERNAME=root,
-    DB_PASSWORD=root,
-    JWT_SECRET: GkfNI09qyNEnyhxn1eHyQ1xWMX9HcR15DuQCW95rhISuIj9ESP
+    PORT: process.env.PORT,
+    HOST: process.env.DB_HOST,
+    DATABASE: process.env.DB_DATABASE,
+    DB_PORT: process.env.DB_PORT,
+    USERNAME: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
+    JWT_SECRET: process.env.JWT_SECRET
 };
 
 // send help 
