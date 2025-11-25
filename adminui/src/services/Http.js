46bd3589fc5b5b96e824c103f5c21833ae43b0f7
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2025-11-25 19:19:19 +0800

    TASK 007 (admin dashboard, validation) - Fixing admin index.html, adjusting http.js on admin UI
    - James David Guba

--- adminui/src/services/Http.js
+++ adminui/src/services/Http.js
@@ -1,20 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
-import { toast } from "react-toastify"; 
-import { useNavigate } from "react-router-dom";
-
-const baseURL = "http://localhost:3000"; 
-
-// Create axios instance
-export const Http = axios.create({
-  baseURL: baseURL,
-  headers: { "Content-Type": "application/json" },
-});
-
-// Add token to headers
-const addAuthToken = (config) => {
-  const token = localStorage.getItem("authToken"); 
-=======
 
 const getAuthToken = () => localStorage.getItem("authToken");
 const baseURL = "localhost:3000";
@@ -27,26 +11,17 @@ export const Http = axios.create({
 //ako kidungang ang http og ImageHttp
 const addAuthToken = (config) => {
   const token = getAuthToken();
->>>>>>> origin/main
   if (token) {
     config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
-<<<<<<< HEAD
-=======
   // Uncomment this if you use FormData uploads
->>>>>>> origin/main
   // if (config.data instanceof FormData) {
   //   config.headers["Content-Type"] = "multipart/form-data";
   // }
   return config;
 };
-<<<<<<< HEAD
-
-// Handle expired token
-=======
 //para automatic logout if expired ang token
->>>>>>> origin/main
 const handleExpiryToken = (error) => {
   const status = error.response?.status;
 
@@ -56,24 +31,12 @@ const handleExpiryToken = (error) => {
     localStorage.removeItem("authToken");
     localStorage.removeItem("user");
 
-<<<<<<< HEAD
-    toast.error("Session Expired. Please log in again."); // Toast message for web
-
-    // Redirect to login page (you can also use context or props)
-=======
     alert("Session Expired. Please log in again.");
->>>>>>> origin/main
     window.location.href = "/login";
   }
 
   return Promise.reject(error);
 };
 
-<<<<<<< HEAD
-// Apply interceptors
 Http.interceptors.request.use(addAuthToken);
 Http.interceptors.response.use((res) => res, handleExpiryToken);
-=======
-Http.interceptors.request.use(addAuthToken);
-Http.interceptors.response.use((res) => res, handleExpiryToken);
->>>>>>> origin/main
