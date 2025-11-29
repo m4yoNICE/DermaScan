584725b44347beda07e5671474d7cf3edc43f46e
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2025-11-29 23:45:25 +0800

    Task 011 (On-going) - Creating users and Update users
    -James David Guba

---
+++ adminui/src/Pages/home/createUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const createUser = () => {
+  return (
+    <div>createUser</div>
+  )
+}
---
+++ adminui/src/Pages/home/updateUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const updateUser = () => {
+  return (
+    <div>updateUser</div>
+  )
+}
